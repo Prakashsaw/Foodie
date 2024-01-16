0b4665af6003317e383499a7b5d4afd280aeaf99
Prakash Saw <prakash1999saw@gmail.com>
2024-01-16 15:54:50 +0530

    adding features of filtering top rated restaurant with rating greater than 4

--- README.md
+++ README.md
@@ -16,5 +16,19 @@
  *
  *
  */
+### Import/Export
+ Two types of Export/Import
+    -Default Export/Import
+        export default Components
+        import components from "psth"
+
+    -Named Expport/Import
+        export const Components
+        import {Components} from "path"
 
 
+### React Hooks
+    ->Normal Javascript utility functions.
+        Mostly used Hooks: 
+        -useState() :- Super powerful state Varaible in react.
+        -useEffect()
--- client/src/components/Body.js
+++ client/src/components/Body.js
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import RestuarantCard from "./RestuarantCard";
 import SearchBar from "./Search";
-import resList from './../utils/resList';
+import resList from "./../utils/resList";
 
 const Body = () => {
+  const [listOfRestaurant, setListOfRestaurant] = useState(resList);
   return (
     <div className="body">
       <div className="search">
         <SearchBar />
       </div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfRestaurant.filter(
+              (res) => res.info.avgRating >= 4
+            );
+            setListOfRestaurant(filteredList);
+            console.log("Button clicked...!");
+          }}
+        >
+          {" "}
+          Top Rated Restaurants
+        </button>
+      </div>
       <div className="res-container">
-        {resList.map((restaurant, index) => (
+        {listOfRestaurant.map((restaurant, index) => (
           <RestuarantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
